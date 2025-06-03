81007f4727985a34f5ec25f8afff8f7e8f954989
davye <davye@uw.edu>
2025-06-03 16:40:00 -0700

    fix dependecy issues

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = Path("README.md").read_text(encoding="utf-8")
 
 setup(
     name="pyshred",
-    version="1.0.4",
+    version="1.0.0",
     author="Kutz Research Group",
     author_email="pyshred1@gmail.com",
     description="PySHRED: A Python Package for SHallow REcurrent Decoders (SHRED) for Spatial-Temporal Systems",
@@ -14,17 +14,16 @@ setup(
     packages=find_packages(),
     license = "BSD-3-Clause",
     install_requires=[
-        "numpy>=1.21,<2.0",
-        "scipy<1.13",
+        "pysindy",
+        "numpy<2.0",
+        "scikit-learn",
         "pandas",
         "torch",
-        "scikit-learn",
-        "pysindy",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires=">=3.8",
 )
