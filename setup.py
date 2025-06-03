b8d59e64f3d84ca6ac8afd3d94fc6c35b16058a7
davye <davye@uw.edu>
2025-06-03 13:18:44 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+long_description = Path("README.md").read_text(encoding="utf-8")
 
 setup(
     name="pyshred",
@@ -6,8 +9,10 @@ setup(
     author="Kutz Research Group",
     author_email="pyshred1@gmail.com",
     description="PySHRED: A Python Package for SHallow REcurrent Decoders (SHRED) for Spatial-Temporal Systems",
+    long_description=long_description,
+    long_description_content_type="text/markdown",   # 'text/x-rst' for reStructuredText
     packages=find_packages(),
-    license = "MIT",
+    license = "BSD-3-Clause",
     install_requires=[
         "numpy",
         "pandas",
