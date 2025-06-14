dc4a8819a5db83430366e017a8ba419e64db9d2e
davye <davye@uw.edu>
2025-06-14 04:57:25 -0700

    update versioning

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@ import os
 project = 'PySHRED'
 copyright = '2025, Kutz Research Group'
 author = 'Kutz Research Group'
-version = release = os.getenv("SPHINX_MULTIVERSION_NAME", "local")
+version = release = os.getenv("SPHINX_MULTIVERSION_NAME")
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
